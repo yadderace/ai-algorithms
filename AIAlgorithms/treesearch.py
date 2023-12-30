583b10d8a5c5338478eb9f41e2cffe53bddd4f8d
Yadder Aceituno <yadderace@gmail.com>
2023-12-30 15:30:01 -0600

    Creating the simple tree search function

---
+++ AIAlgorithms/treesearch.py
@@ -0,0 +1,33 @@
+from queue import Queue
+
+frontier = Queue()
+def simple_treesearch(problem, space):
+
+    # Initializing the frontier
+    print(problem['init_state_id'])
+    initial_state_node = space.nodes[problem['init_state_id']]
+    print(initial_state_node)
+    if initial_state_node == None:
+        return { 'error': True, 'message': 'The initial state does not exist', 'solution': None }
+    
+    # Adding neighbor nodes to the frontier
+    [frontier.put(neighbor) for neighbor in space.neighbors(initial_state_node.node_id)]
+
+    goal_state_id = problem['end_state_id']
+    
+    while True:
+
+        # If frontier is empty there's no solution
+        if frontier.qsize() == 0:
+            return { 'error': True, 'message': 'There is not solution', 'solution': None }
+        
+        # Validating if the next node of frontier is the goal
+        leaf_node = frontier.get()
+        if leaf_node.node_id == goal_state_id:
+            return { 'error': False, 'message': 'Solution Found', 'solution': leaf_node}
+        
+        # Expanding leaf node and adding them to the frontier
+        [frontier.put(neighbor) for neighbor in space.neighbors(initial_state_node.node_id)]
+        
+
+        
