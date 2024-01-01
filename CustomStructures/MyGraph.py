5e77f99648210c983df8ab677b5b2522665b6686
Yadder Aceituno <yadderace@gmail.com>
2024-01-01 16:43:40 -0600

    Adding node and path cost attributes to graph node

--- CustomStructures/MyGraph.py
+++ CustomStructures/MyGraph.py
@@ -4,7 +4,12 @@ class GraphNode():
 
     def __init__(self, node_id, custom_attributes):
         self.node_id = node_id
+        self.path_cost = 0
+        self.neighbor_nodes = {}
         self.custom_attributes = custom_attributes
+
+    def add_neighbor_node(self, node_id, node):
+        self.neighbor_nodes[node_id] = node
 
 
 class MyGraph(nx.Graph):
@@ -15,3 +20,14 @@ class MyGraph(nx.Graph):
     def add_node(self, node_id, custom_attributes=None):
         node = GraphNode(node_id, custom_attributes)
         super().add_node(node_id, data=node)
+
+    def add_edges_from(self, ebunch_to_add, **attr):
+        
+        for edge in ebunch_to_add:
+            node_id1, node_id2 = edge
+            node1 = self.nodes[node_id1]['data']
+            node2 = self.nodes[node_id2]['data']
+            node1.add_neighbor_node(node_id1, node1)
+            node2.add_neighbor_node(node_id2, node2)
+
+        return super().add_edges_from(ebunch_to_add, **attr)
