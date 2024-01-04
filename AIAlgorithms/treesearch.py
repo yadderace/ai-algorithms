ac0308638b49cf8d193f2d61103bb098c5114428
Yadder Aceituno <yadderace@gmail.com>
2024-01-04 00:08:14 -0600

    Adding weights to edges and creating bread first search algorithm

--- AIAlgorithms/treesearch.py
+++ AIAlgorithms/treesearch.py
@@ -58,3 +58,41 @@ def simple_graphsearch(problem, space):
 
         # Expanding leaf node and adding them to the frontier if the node isn't in the explored nodes or frontier
         [frontier.put(neighbor) for neighbor in space.neighbors(leaf_node.node_id) if (neighbor not in explored_nodes) and (neighbor not in list(frontier.queue))]
+
+
+def breadth_first_search(problem, space):
+
+    initial_state_node = space.nodes[problem['init_state_id']]['data']
+    goal_state_id = problem['end_state_id']
+
+    # Validating if initial state is goal node
+    if(initial_state_node.node_id == goal_state_id):
+        return { 'error': False, 'message': 'Solution Found', 'solution': initial_state_node}
+    
+    # Initializing frontier and explored nodes
+    frontier = Queue()
+    frontier.put(initial_state_node.node_id)
+    explored_nodes = []
+
+    while True:
+
+        # Validating if there's any node in the frontier
+        if frontier.qsize() == 0:
+            return { 'error': True, 'message': 'There is not solution', 'solution': None }
+        
+        # Getting a node to validate from the frontier
+        current_node = frontier.get()
+        explored_nodes.append(current_node)
+
+        # Passing through leaf nodes from current_node
+        for child_node_id in space.neighbors(current_node):
+            
+            leaf_node = space.nodes[child_node_id]['data']
+            if(leaf_node.node_id not in explored_nodes) and (leaf_node.node_id not in list(frontier.queue)):
+                
+                print(f'Passing through Node ID: {leaf_node.node_id}, attributes: {leaf_node.custom_attributes}')
+                if(leaf_node.node_id == goal_state_id):
+                    return { 'error': False, 'message': 'Solution Found', 'solution': leaf_node}
+                
+                frontier.put(leaf_node.node_id)
+    
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import CustomStructures.MyGraph as cg
 
-from AIAlgorithms.treesearch import simple_treesearch, simple_graphsearch
+from AIAlgorithms.treesearch import simple_treesearch, simple_graphsearch, breadth_first_search
 
 
 # ========================================== [DEFINING SPACE]
@@ -27,29 +27,69 @@ problem_space.add_node(19, custom_attributes={'name': 'Hirsova'})
 problem_space.add_node(20, custom_attributes={'name': 'Eforie'})
 
 problem_space.add_edges_from([(1,2), (1,8)])
+problem_space[1][2]['weight'] = 71
+problem_space[1][8]['weight'] = 151
+
 problem_space.add_edges_from([(2,3)])
+problem_space[2][3]['weight'] = 75
+
 problem_space.add_edges_from([(3,4), (3,8)])
+problem_space[3][4]['weight'] = 118
+problem_space[3][8]['weight'] = 140
+
 problem_space.add_edges_from([(4,5)])
+problem_space[4][5]['weight'] = 111
+
 problem_space.add_edges_from([(5,6)])
+problem_space[5][6]['weight'] = 70
+
 problem_space.add_edges_from([(6,7)])
+problem_space[6][7]['weight'] = 75
+
 problem_space.add_edges_from([(7,10)])
+problem_space[7][10]['weight'] = 120
+
 problem_space.add_edges_from([(8,9), (8,11)])
+problem_space[8][9]['weight'] = 80
+problem_space[8][11]['weight'] = 99
+
 problem_space.add_edges_from([(9,10), (9, 12)])
+problem_space[9][10]['weight'] = 146
+problem_space[9][12]['weight'] = 97
+
 problem_space.add_edges_from([(10,12)])
+problem_space[10][12]['weight'] = 138
+
 problem_space.add_edges_from([(11,13)])
+problem_space[11][13]['weight'] = 211
+
 problem_space.add_edges_from([(12,13)])
+problem_space[12][13]['weight'] = 101
+
 problem_space.add_edges_from([(13,14), (13,18)])
+problem_space[13][14]['weight'] = 90
+problem_space[13][18]['weight'] = 85
+
 problem_space.add_edges_from([(15,16)])
+problem_space[15][16]['weight'] = 87
+
 problem_space.add_edges_from([(16,17)])
+problem_space[16][17]['weight'] = 92
+
 problem_space.add_edges_from([(17,18)])
+problem_space[17][18]['weight'] = 142
+
 problem_space.add_edges_from([(18,19)])
+problem_space[18][19]['weight'] = 98
+
 problem_space.add_edges_from([(19,20)])
+problem_space[19][20]['weight'] = 86
 
 # ========================================== [SIMPLE TREE SEARCH]
 print("======================== [Simple Tree Search]")
 problem = {
     'init_state_id': 3,
-    'end_state_id': 12
+    'end_state_id': 13
 }
 print(f"Problem: {problem}")
 solution = simple_treesearch(problem=problem, space=problem_space)
@@ -59,8 +99,18 @@ print(f"Solution {solution}")
 print("======================== [Simple Graph Search]")
 problem = {
     'init_state_id': 3,
-    'end_state_id': 12
+    'end_state_id': 13
 }
 print(f"Problem: {problem}")
 solution = simple_graphsearch(problem=problem, space=problem_space)
+print(f"Solution {solution}")
+
+# ========================================== [BREADTH FIRST SEARCH]
+print("======================== [Breadth First Search]")
+problem = {
+    'init_state_id': 3,
+    'end_state_id': 13
+}
+print(f"Problem: {problem}")
+solution = breadth_first_search(problem=problem, space=problem_space)
 print(f"Solution {solution}")
