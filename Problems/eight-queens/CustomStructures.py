ebc4582c88a9eea9bc1214a008e00eb238326698
Yadder Aceituno <yadderace@gmail.com>
2024-02-10 19:45:23 -0600

    Adding methods to create child alternatives

---
+++ Problems/eight-queens/CustomStructures.py
@@ -0,0 +1,39 @@
+from treelib import Node, Tree
+
+
+class MyTreeNode(Node):
+    def __init__(self, tag, identifier=None, node_id = None, data=None, tree=None, g_cost = 0, h_cost=0):
+        super().__init__(tag=tag, identifier=identifier, data=data)
+        self.node_id = node_id
+        self.g_cost = g_cost
+        self.h_cost = h_cost
+        self.f_cost = g_cost + h_cost
+        self.tree = tree
+    
+    # def add_child(self, tag, node_id = None, data=None, g_cost=0, h_cost=0):
+    #     if(self.tree is not None):
+    #         child_node = MyTreeNode(tag=tag, identifier=self.tree.nodes, node_id = node_id, data=data, g_cost=g_cost, h_cost=h_cost, tree=self.tree)
+    #         self.tree.add_node(child_node, parent = self.identifier)
+    #         self.tree.nodes += 1
+
+
+
+class MyTree(Tree):
+    def __init__(self, tree_id=None):
+        super().__init__(tree_id)
+        # Add custom attributes or methods as needed
+        self.added_nodes = 0
+
+    def create_node(self, tag, node_id = None, parent=None, data=None, g_cost=0, h_cost=0):
+        node = MyTreeNode(tag=tag, identifier=self.added_nodes, node_id = node_id, data=data, g_cost=g_cost, h_cost=h_cost, tree=self)
+        self.add_node(node, parent)
+        self.added_nodes += 1
+        return node
+
+    @property
+    def nodes(self):
+        return self.added_nodes
+
+    @nodes.setter
+    def nodes(self, new_value):
+        self.added_nodes = new_value
--- Problems/eight-queens/Problem.py
+++ Problems/eight-queens/Problem.py
@@ -1,27 +1,61 @@
 from ChessBoard import ChessBoard  # Assuming your ChessBoard class is defined in chess_board.py
 
+from CustomStructures import MyTree
+
 class Problem:
+    
     def __init__(self, n_dim, n_queens):
         self.n_dim = n_dim
         self.n_queens = n_queens
+        
+
+    def start_board(self):
+        # Initializing the board
         self.chess_board = ChessBoard(n_dim, n_queens)
         self.chess_board.place_queens()  # Place the queens
-        self.current_state_identifier = self.chess_board.get_identifier()
+        current_state_identifier = self.chess_board.get_identifier()
+        
+        # Creating the tree
+        self.tree_space = MyTree()
+        self.current_node = self.tree_space.create_node('Root', node_id=current_state_identifier, g_cost=0, h_cost=0)
+    
 
     def print_current_state(self):
-        print("Current State:")
-        self.chess_board.print_board_from_identifier(self.current_state_identifier)
+        self.chess_board.print_board_from_identifier(self.current_node.node_id)
 
-        next_moves = self.chess_board.get_queen_next_moves(3)
-        for move in next_moves:
-            print("=============================================")
-            self.chess_board.print_board_from_identifier(move)
-            print(f"Cost {self.chess_board.cost_queens(move)}")
+    def create_child_nodes(self, tree_node=None):
+        
+        if(tree_node is None):
+            tree_node = self.tree_space.get_node(self.current_node.identifier)
+        
+        child_objects = []
+        
+        # Iterate through each queen
+        for i in range(1, self.n_queens + 1):
+            queen_next_moves = self.chess_board.get_queen_next_moves(i)
+            
+            
+            for next_identifier in queen_next_moves:
+                # Calculate the cost for the new state
+                new_cost = self.chess_board.cost_queens(next_identifier)
+
+                # Append the alternative identifier and cost to the list of child nodes
+                child_objects.append({'alternative_identifier': next_identifier, 'cost': new_cost})
+                
+                # Create a child on the tree
+                self.tree_space.create_node('Child', node_id=next_identifier, parent=tree_node.identifier, g_cost=0, h_cost=new_cost)
+
+        return child_objects
 
 # Example usage:
 if __name__ == "__main__":
     n_dim = 8
-    n_queens = 5
+    n_queens = 8
     problem_instance = Problem(n_dim, n_queens)
+    
+    problem_instance.start_board()
     problem_instance.print_current_state()
+
+    list_next_moves = problem_instance.create_child_nodes()
+
 
